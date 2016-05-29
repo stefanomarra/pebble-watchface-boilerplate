31c9f85159bede04f97e04c11baf25c77034ca4d
Stefano Marra <stefano.marra1987@gmail.com>
2016-05-29 02:26:02 +0200

    Add battery state service and handler

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_battery_layer;
 
 static void update_time() {
 
@@ -10,16 +11,31 @@ static void update_time() {
 	struct tm *tick_time = localtime(&temp);
 
 	// Write the current hours and minutes into a buffer
-	static char s_buffer[10];
-	strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ? "%H:%M:%S" : "%I:%M:%S", tick_time);
+	static char time_text[10];
+	strftime(time_text, sizeof(time_text), clock_is_24h_style() ? "%H:%M:%S" : "%I:%M:%S", tick_time);
 
 	// Display this time on the TextLayer
-	text_layer_set_text(s_time_layer, s_buffer);
+	text_layer_set_text(s_time_layer, time_text);
 
 }
 
 static void handle_second_tick(struct tm *tick_time, TimeUnits units_changed) {
 	update_time();
+}
+
+static void handle_battery(BatteryChargeState charge_state) {
+	static char battery_text[] = "100% charged";
+
+	// Change battery text layer if in charge, otherwise change battery percentage
+	if (charge_state.is_charging) {
+		snprintf(battery_text, sizeof(battery_text), "charging");
+	}
+	else {
+		snprintf(battery_text, sizeof(battery_text), "%d%% charged", charge_state.charge_percent);
+	}
+
+	// Update battery layer
+	text_layer_set_text(s_battery_layer, battery_text);
 }
 
 static void setup_time_layers() {
@@ -48,6 +64,30 @@ static void setup_time_layers() {
 
 static void setup_date_layers() {}
 
+static void setup_battery_layer() {
+
+	// Get information about the window
+	Layer *window_layer = window_get_root_layer(s_main_window);
+	GRect bounds = layer_get_bounds(window_layer);
+
+	// Create the TextLayer with specific bounds
+	s_battery_layer = text_layer_create(
+		GRect(0, PBL_IF_ROUND_ELSE(88, 82), bounds.size.w, 80));
+
+	// Style the TextLayer
+	text_layer_set_background_color(s_battery_layer, GColorClear);
+	text_layer_set_text_color(s_battery_layer, GColorWhite);
+	text_layer_set_text(s_battery_layer, "100% charged");
+	text_layer_set_font(s_battery_layer, fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS));
+	text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
+
+	// Add it as child layer to the Window's root layer
+	layer_add_child(window_layer, text_layer_get_layer(s_battery_layer));
+
+	// Update initial battery
+	handle_battery(battery_state_service_peek());
+}
+
 static void main_window_load(Window *window) {
 
 	/**
@@ -56,12 +96,16 @@ static void main_window_load(Window *window) {
 	 */
 	window_set_background_color(window, GColorSunsetOrange);
 
-	// Setup time/date layers
+	// Setup time/date/battery layers
 	setup_time_layers();
 	setup_date_layers();
+	setup_battery_layer();
 
 	// Register TickTimerService
 	tick_timer_service_subscribe(SECOND_UNIT, handle_second_tick);
+
+	// Register BatteryStateService
+	battery_state_service_subscribe(handle_battery);
 }
 
 static void main_window_unload(Window *window) {
@@ -71,6 +115,9 @@ static void main_window_unload(Window *window) {
 
 	// Unregister TickTimerService
 	tick_timer_service_unsubscribe();
+
+	// Unregister BatteryStateService
+	battery_state_service_unsubscribe();
 }
 
 static void init() {
