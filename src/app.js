021aee52cea3855c3296087a9bb810becc71a13e
Stefano Marra <stefano.marra1987@gmail.com>
2016-05-30 03:57:21 +0200

    Add initial configurable files

---
+++ src/app.js
@@ -0,0 +1,5 @@
+Pebble.addEventListener('showConfiguration', function() {
+	var url = 'https://rawgit.com/stefanomarra/pebble-watchface-boilerplate/blob/master/config/index.html';
+
+	Pebble.openURL(url);
+});
