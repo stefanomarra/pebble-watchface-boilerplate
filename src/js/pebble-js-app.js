a90d7ee8803f8041e5c3bc91664e57590e152155
Stefano Marra <stefano.marra1987@gmail.com>
2016-06-02 15:56:23 +0200

    Update pebble app js

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -13,21 +13,23 @@ Pebble.addEventListener('webviewclosed', function(e) {
 	console.log('Configuration page returned: ' + JSON.stringify(configData));
 
 	var dict = {};
-	dict['KEY_COLOR_BACKGROUND']  = parseInt(configData['background_color'].substring(0), 16);
-	dict['KEY_COLOR_TIME']        = parseInt(configData['time_text_color'].substring(0), 16);
-	dict['KEY_COLOR_DATE']        = parseInt(configData['date_text_color'].substring(0), 16);
-	dict['KEY_COLOR_HOUR_HAND']   = parseInt(configData['hour_hand_color'].substring(0), 16);
-	dict['KEY_COLOR_MINUTE_HAND'] = parseInt(configData['minute_hand_color'].substring(0), 16);
-	dict['KEY_COLOR_SECOND_HAND'] = parseInt(configData['second_hand_color'].substring(0), 16);
-	dict['KEY_SHOW_HANDS']        = configData['show_hands'] ? 1 : 0;
-	dict['KEY_SHOW_SECOND_HAND']  = configData['show_second_hand'] ? 1 : 0;
-	dict['KEY_SHOW_TIME']         = configData['show_time'] ? 1 : 0;
-	dict['KEY_SHOW_DATE']         = configData['show_date'] ? 1 : 0;
+	dict.KEY_COLOR_BACKGROUND  = parseInt(configData.background_color, 16);
+	dict.KEY_COLOR_TIME        = parseInt(configData.time_text_color, 16);
+	dict.KEY_COLOR_DATE        = parseInt(configData.date_text_color, 16);
+	dict.KEY_COLOR_HOUR_HAND   = parseInt(configData.hour_hand_color, 16);
+	dict.KEY_COLOR_MINUTE_HAND = parseInt(configData.minute_hand_color, 16);
+	dict.KEY_COLOR_SECOND_HAND = parseInt(configData.second_hand_color, 16);
+	dict.KEY_SHOW_HANDS        = configData.show_hands ? 1 : 0;
+	dict.KEY_SHOW_SECOND_HAND  = configData.show_second_hand ? 1 : 0;
+	dict.KEY_SHOW_TIME         = configData.show_time ? 1 : 0;
+	dict.KEY_SHOW_DATE         = configData.show_date ? 1 : 0;
+
+	console.log('Configuration page returned: ' + JSON.stringify(dict));
 
 	// Send to watchapp
 	Pebble.sendAppMessage(dict, function() {
 		console.log('Send successful: ' + JSON.stringify(dict));
-	}, function() {
-		console.log('Send failed!');
+	}, function(e) {
+		console.log('Send failed! Unable to deliver message with transactionId=' + e.data.transactionId + ' Error is: ' + e.error.message);
 	});
 });
